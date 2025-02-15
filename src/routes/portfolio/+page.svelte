859c4d76d7553e50c7dbdc17851988eea43852d3
dastin <astindillon@gmail.com>
2025-02-14 21:55:26 -0500

    updated portfolio + new images

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from "$lib/components/header.svelte";
-  import website from "$lib/assets/img/website.png";
+  import website from "$lib/assets/img/website.webp";
+  import breakout from "$lib/assets/img/breakout.webp"
 
   const title = "Portfolio";
   const description = `
@@ -14,8 +15,8 @@
 
   <div class="divider my-8"></div>
 
-  <div class="flex flex-col space-y-6">
-    <div class="card lg:card-side bg-base-200 shadow-xl">
+  <div class="flex flex-col space-y-20 mx-20">
+    <div class="card sm:card-side bg-base-200 shadow-xl">
       <figure>
         <img src={website} alt="screenshot of my website"/>
       </figure>
@@ -27,8 +28,24 @@
         </div>
       </div>
     </div>
-  </div>
 
+    <div class="card sm:card-side bg-base-200 shadow-xl">
+      <figure>
+        <img src={breakout} alt="screenshot of breakout game"/>
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Breakout Clone</h2>
+        <p>
+          I made a breakout clone as a Valentines Day gift for my wife.
+        </p>
+        <p>It's built using Phaser 3</p>
+        <div class="card-actions justify-end">
+          <a class="btn btn-primary" href="https://v-day-game.vercel.app">Visit Site</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </div>
 
 
